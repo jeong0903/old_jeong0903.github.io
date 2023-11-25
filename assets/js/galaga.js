7d659f7d47c0020e4e3bd26266efe8660ef86459
Jane Park <9193jane@gmail.com>
2023-11-25 17:08:54 +0900

    20231125 galaga-4

--- assets/js/galaga.js
+++ assets/js/galaga.js
@@ -5,10 +5,12 @@ const galagaMain = document.querySelector('.galagaMain');
 canvas = document.createElement("canvas");
 ctx = canvas.getContext("2d");
 canvas.width = 400;
-canvas.height = 700;
+canvas.height = 600;
 galagaMain.appendChild(canvas);
 
-let bgImg, jetImg, bulletImg, enemyImg, ggImg;
+let bgImg, jetImg, bulletImg, enemyImg, ggImg, bombImg;
+let gameOver = false;
+let score = 0;
 
 // 우주선 좌표 (유동적)
 let jetX = canvas.width/2-32;
@@ -24,11 +26,49 @@ function Bullet(){
   this.init = function(){
     this.x = jetX + 20;
     this.y = jetY;
-
-    bulletList.push(this);
-  }
+    this.alive = true;  // true면 살아있는 총알, false면 죽은 총알
+    // 총알 4. 모든 총알들은 각각의 X, Y 좌표값을 가짐
+    bulletList.push(this);  // push(this) : this.x, this.y, this.init
+  };
   this.update = function(){
     this.y -= 7;
+  };
+
+  this.checkHit = function(){
+    // 총알.y <= 적군.y and
+    // 총알.x >= 적군.x and 총알.x <= 적군.x + 적군의 넓이
+    for (let i = 0; i < enemyList.length; i++) {
+      if(this.y <= enemyList[i].y && this.x >= enemyList[i].x && this.x <= enemyList[i].x+50 ){
+        score += 10;
+        this.alive = false;   // 죽은 총알
+        enemyList.splice(i, 1);
+      }
+    }
+  }
+}
+
+function generateRandomValue(min, max) {
+  let randomNum = Math.floor(Math.random()*(max-min+1)) + min;
+  return randomNum;
+}
+
+// 적군 생성
+let enemyList = [];
+function Enemy(){
+  this.x = 0;
+  this.y = 0;
+  this.init = function(){
+    this.y = 0;
+    this.x = generateRandomValue(0, canvas.width-58);
+
+    enemyList.push(this);  
+  }
+  this.update = function(){
+    this.y += 3;
+
+    if(this.y >= canvas.height-40){
+      gameOver = true;
+    }
   }
 }
 
@@ -78,12 +118,20 @@ function createBullet(){
   console.log("총알리스트", bulletList);
 }
 
-// 우주선 2. 우주선의 X, Y 좌표가 바뀌고,
+function createEnemy(){
+  const interval = setInterval(function(){
+    let e = new Enemy();  // 적군 하나 생성
+    e.init();
+    console.log("적군리스트", enemyList);
+  }, 1000)
+}
+
 function update(){
+  // 우주선 2. 우주선의 X, Y 좌표가 바뀌고,
   if( 'ArrowRight' in keysDown) {
-    jetX += 3;
+    jetX += 4;
   } else if('ArrowLeft' in keysDown) {
-    jetX -= 3;
+    jetX -= 4;
   };
   // 우주선 좌표값 제한(캔버스 안에서만 무빙)
   if(jetX <= 0){
@@ -94,8 +142,14 @@ function update(){
 
   // 총알의 y 좌표 업데이트 함수 호출
   for (let i = 0; i < bulletList.length; i++) {
-    bulletList[i].update();
-    
+    if(bulletList[i].alive){
+      bulletList[i].update();
+      bulletList[i].checkHit();
+    }
+  }
+
+  for (let i = 0; i < enemyList.length; i++) {
+    enemyList[i].update();
   }
 }
 
@@ -104,24 +158,42 @@ function render(){
   // 우주선 3. 다시 RENDER 그려준다.
   ctx.drawImage(jetImg, jetX, jetY);
 
+  ctx.fillText(`SCORE:${score}`, 20, 40);
+  ctx.fillStyle =   "white";
+  ctx.font = "30px bold S-CoreDream-3Light";
+
+  // 총알 5. 총알 배열을 가지고 렌더
   for (let i = 0; i < bulletList.length; i++) {
-    ctx.drawImage(bulletImg, bulletList[i].x, bulletList[i].y, 25, 25)
-    
+    if(bulletList[i].alive){
+      ctx.drawImage(bulletImg, bulletList[i].x, bulletList[i].y, 25, 25)
+    }
   }
+
+  for (let i = 0; i < enemyList.length; i++) {
+    ctx.drawImage(enemyImg, enemyList[i].x, enemyList[i].y, 50, 50)
+  }
+
 }
 
 function main(){
+  if(!gameOver){
   update();
   render();
   requestAnimationFrame(main);
+}else{
+  ctx.drawImage(ggImg, canvas.width/2-100, 100, 200, 180);
+}
 }
 
 loadImage();
 setupKeyboardListener();
+createEnemy();
 main();
 
 
 
-
-// 총알 4. 모든 총알들은 X, Y 좌표값을 가짐
-// 총알 5. 총알 배열을 가지고 렌더
+// 적군 1. 위치가 랜덤
+// 적군 2. 밑으로 내려감
+// 적군 3. 주기적으로 나타남
+// 적군 4. 바닥에 닿으면 게임 끝
+// 적군 5. 총알이 닿으면 bomb, score += 1
